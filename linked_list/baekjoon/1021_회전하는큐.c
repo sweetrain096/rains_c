f2ae7840bf441b47bea42e4cb95536e5be90cd17
sweetrain096 <gpfhddl09@gmail.com>
2019-04-24 18:47:34 +0900

    190424|회전하는큐미완

---
+++ linked_list/baekjoon/1021_회전하는큐.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "malloc.h"
+int n, m;
+int check_list[60];
+
+typedef struct node {
+	int data;
+	int number;
+	struct node *next;
+}node;
+node *head;
+node *tail;
+
+void insert_node() {
+
+}
+int main() {
+	freopen("1021_input.txt", "r", stdin);
+	int i;
+	scanf("%d %d", &n, &m);
+	for (i = 0; i< m)
+	return 0;
+}
