b5f4d130af4c864a15b1451524c7446d63293d6d
sweetrina096 <gpfhddl09@gmail.com>
2020-11-02 22:13:54 +0900

    기초 다지기

---
+++ BAEKJOON_단계별로 풀어보기/입출력/2588.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+	int num1, num2;
+	scanf("%d %d", &num1, &num2);
+	printf("%d\n", num1*(num2 % 10));
+	printf("%d\n", num1*((num2 % 100) / 10));
+	printf("%d\n", num1*(num2 / 100));
+	printf("%d", num1*num2);
+	return 0;
+}
